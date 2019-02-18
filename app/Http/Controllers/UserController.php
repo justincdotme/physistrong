37b084fc5a4273aa5280b5258888d008ac55630d
Justin Christenson <info@justinc.me>
2019-02-17 19:16:21 -0800

    PS-11 Adding user login feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,24 +3,82 @@
 namespace App\Http\Controllers;
 
 use App\Core\User;
-use Illuminate\Http\Request;
+use App\Exceptions\Errors\JsonApi;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\UserRegistrationConfirmation;
 use App\Http\Resources\User as UserResource;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
-        $this->authorizeResource(User::class, 'user');
-    }
-
     /**
      * Display the specified resource.
      *
      * @param User $user
-     * @return User
+     * @return UserResource
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
     public function show(User $user)
     {
+        $this->authorize('view', [User::class, $user]);
         return new UserResource($user);
+    }
+
+    /**
+     * Register a new user
+     *
+     * @return UserResource
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store()
+    {
+        $this->validate(
+            request(),
+            [
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:6', 'confirmed']
+            ]
+        );
+
+        $user = User::create([
+            'email' => request('email'),
+            'first_name' => request('first_name'),
+            'last_name' => request('last_name'),
+            'password' => bcrypt(request('password'))
+        ]);
+
+        Mail::to($user->email)->send(new UserRegistrationConfirmation($user));
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Log a user in.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login()
+    {
+        $this->validate(
+            request(),
+            [
+                'email' => 'required',
+                'password' => 'required'
+            ]
+        );
+
+        if (! $token = JWTAuth::attempt(request(['email', 'password']))) {
+            return response()->json(
+                JsonApi::formatError(401, request()->path(), "Authorization failed."),
+                401
+            );
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => (auth()->factory()->getTTL() * 60)
+        ]);
     }
 }
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Core\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $existingUser;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->existingUser = factory(User::class)->create([
+            'email' => 'test.user@physistrong.com',
+            'password' => bcrypt('testing123')
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function unauthenticated_user_can_login()
+    {
+        $this->withoutExceptionHandling();
+        $this->response = $this->json("POST", route('user.login'), [
+            'email' => 'test.user@physistrong.com',
+            'password' => 'testing123'
+        ]);
+
+        $this->response->assertStatus(200);
+        $this->response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in'
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function authenticated_users_can_not_login()
+    {
+        $this->response = $this->actingAs($this->existingUser)->json("POST", route('user.login'), [
+            'email' => 'test.user@physistrong.com',
+            'password' => 'testing123'
+        ]);
+
+        $this->response->assertStatus(409);
+        $responseArray = $this->response
+            ->decodeResponseJson();
+        $this->assertArrayHasKey('errors', $responseArray);
+        $this->assertEquals('409', $responseArray['errors']['status']);
+        $this->assertEquals('The user is already authenticated.', $responseArray['errors']['detail']);
+    }
+
+    /**
+     * @test
+     */
+    public function unregistered_users_can_not_login()
+    {
+        $this->response = $this->json("POST", route('user.login'), [
+            'email' => 'unknown.user@physistrong.com',
+            'password' => 'foopass'
+        ]);
+
+        $this->response->assertStatus(401);
+        $responseArray = $this->response
+            ->decodeResponseJson();
+        $this->assertArrayHasKey('errors', $responseArray);
+        $this->assertEquals('401', $responseArray['errors']['status']);
+        $this->assertEquals('Authorization failed.', $responseArray['errors']['detail']);
+    }
+
+    /**
+     * @test
+     */
+    public function email_is_required()
+    {
+        $this->response = $this->json("POST", route('user.login'), [
+            'password' => 'testing123'
+        ]);
+
+        $this->assertFieldHasValidationError('email');
+    }
+
+    /**
+     * @test
+     */
+    public function password_is_required()
+    {
+        $this->response = $this->json("POST", route('user.login'), [
+            'email' => 'test.user@physistrong.com'
+        ]);
+
+        $this->assertFieldHasValidationError('password');
+    }
+}
