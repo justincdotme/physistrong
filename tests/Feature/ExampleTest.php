ee319cd1051585f18e0c13be59f603ac85d1ef42
Justin Christenson <info@justinc.me>
2026-06-28 03:37:38 +0000

    PS-55 - Fixing test for see eye

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/health');
 
         $response->assertStatus(200);
     }
